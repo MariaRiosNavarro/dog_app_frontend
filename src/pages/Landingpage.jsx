00915618de4462759861da5645892389f81a346b
SuperCoder <propscode254@gmail.com>
2023-12-19 10:28:04 +0100

    add config

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Landingpage = () => {
   return (
     <main>
       <section>
-        <h1>DogDate</h1>
+        <div>
+          <h1>DogDate</h1>
+          <p>Der Hundekuppler</p>
+        </div>
+      </section>
+      <section>{/* componente */}</section>
+      <section>
+        <img src="" alt="" />
+        <div>
+          <p>Bring dein Hund</p>
+          <Link to="/add">Lets DogDate</Link>
+        </div>
       </section>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,34 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   daisyui: {
-    themes: ["dark"],
+    themes: ["lemonade", "dark", "emerald"],
   },
-  theme: {
-    extend: {
-      colors: {
-        bgColor: "#283A45",
-        primaryC: "#2A9D8F",
-        secondaryC: "#A16171",
-        accentC: "#E9C46A",
-      },
-      screens: {
-        smallmobile: "320px",
-        // => @media (min-width: 320px) { ... }
+  theme: { extend: {} },
+  // extend: {
+  //   colors: {
+  //     bgColor: "#283A45",
+  //     primaryC: "#2A9D8F",
+  //     secondaryC: "#A16171",
+  //     accentC: "#E9C46A",
+  //   },
+  //   screens: {
+  //     smallmobile: "320px",
+  //     // => @media (min-width: 320px) { ... }
+  //     mobile: "375px",
+  //     // => @media (min-width: 375px) { ... }
+  //     tablet: "769px",
+  //     // => @media (min-width: 768px) { ... }
+  //     laptop: "900px",
+  //     // => @media (min-width: 1000px) { ... }
+  //     desktop: "1280px",
+  //     // => @media (min-width: 1280px) { ... }
+  //     bigdesktop: "1440px",
+  //   },
+  // },
 
-        mobile: "375px",
-        // => @media (min-width: 375px) { ... }
-
-        tablet: "769px",
-        // => @media (min-width: 768px) { ... }
-
-        laptop: "900px",
-        // => @media (min-width: 1000px) { ... }
-
-        desktop: "1280px",
-        // => @media (min-width: 1280px) { ... }
-        bigdesktop: "1440px",
-      },
-    },
-  },
   plugins: [require("daisyui")],
 };
